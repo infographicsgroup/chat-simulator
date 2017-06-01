28c3ea70b6339372e20d97ab2d05f775aa073e05
Jonas Parnow <jonasparnow@gmail.com>
2017-06-01 19:12:01 +0200

    init

---
+++ src/scripts/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+//import 'babel-polyfill';
+import '../styles/index.scss';
+
+// const number = 123;
+// const output = `A number: ${number}`;
+
+// console.log(output);
